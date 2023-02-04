342f70b35ed5b43a38f45c5efbc0017a209e3ca2
vignesh1112 <57535155+vignesh1112@users.noreply.github.com>
2023-02-04 17:45:56 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String mobileBrandName = "SAMSUNG";
+		char mobileLogo = '$';
+		short noOfMobilePiece = 2;
+		int modeNumber = 4566754;
+		long mobileImeiNumber = 123234343535L;
+		float mobilePrice = 60000;
+		boolean isDamaged = true;
+		
+		System.out.println(mobileBrandName);
+		System.out.println(mobileLogo);
+		System.out.println(noOfMobilePiece);
+		System.out.println(modeNumber);
+		System.out.println(mobileImeiNumber);
+		System.out.println(mobilePrice);
+		System.out.println(isDamaged);
+		
+	}
+
+}
---
+++ TwoWheeler.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class TwoWheeler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int noofWheels = 2;
+		short noofMirrors = 2;
+		long  chassisNumber = 1235565667L;
+		boolean isPunctured = true;
+		String bikeName = "Royal Enfield";
+		double runningKM = 2500;
+		
+		System.out.println(noofWheels);
+		System.out.println(noofMirrors);
+		System.out.println(chassisNumber);
+		System.out.println(isPunctured);
+		System.out.println(bikeName);
+		System.out.println(runningKM);
+
+	}
+
+}
